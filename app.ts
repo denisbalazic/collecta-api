827fcde5329f4b8e013395eb1fef5d209d5191ca
denis balazic <denis.balazic@nobula.eu>
2021-11-19 13:11:05 +0100

    chore: Add environment variables loader
    Resolves: #5

--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import environmentVars from './config/index';
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/collecta').then(
+const { databaseURL, port } = environmentVars;
+
+mongoose.connect(databaseURL).then(
   () => console.log('MongoDB is connected'),
   (err) => console.log(err)
 );
@@ -12,6 +15,6 @@ app.get('/', (req, res) => {
   res.send('Well done!');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('The application is listening on port 3000!');
 });
