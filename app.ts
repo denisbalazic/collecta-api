3193e47850e0908ee53ede9a12a65c0a74418146
denis balazic <denis.balazic@nobula.eu>
2021-11-19 12:31:44 +0100

    chore: Connect to mongodb
    Resolves: #3

--- app.ts
+++ app.ts
@@ -1,6 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/collecta').then(
+  () => console.log('MongoDB is connected'),
+  (err) => console.log(err)
+);
 
 app.get('/', (req, res) => {
   res.send('Well done!');
