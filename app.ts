d2b3f1a6d473531287e096aae9195bdc09e2e10d
denis balazic <denis.balazic@nobula.eu>
2022-02-13 16:55:56 +0100

    feat: Separate server

--- app.ts
+++ app.ts
@@ -1,18 +1,11 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-import { databaseURL, port } from './config';
 import userRoutes from './routes/userRoutes';
 import authRoutes from './routes/authRoutes';
 import collectionRoutes from './routes/collectionRoutes';
 import errorHandler from './middleware/errorHandler';
 
 const app = express();
-
-mongoose.connect(databaseURL).then(
-    () => console.log('MongoDB is connected'),
-    (err) => console.log(err)
-);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
@@ -27,6 +20,4 @@ app.use('/collections', collectionRoutes);
 
 app.use(errorHandler);
 
-app.listen(port, () => {
-    console.log('The application is listening on port 3000!');
-});
+export default app;
